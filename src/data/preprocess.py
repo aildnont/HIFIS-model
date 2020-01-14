ce1065e0e8d5cf4c68ecfbb7429716a1fcbd4cfa
bvanberl <bvanberl@uwo.ca>
2020-01-14 16:14:24 -0500

    Implemented separation of single- and multi-valued categorical features. Implemented generation of mapping of single-valued categorical features to their vectorized values

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -28,26 +28,68 @@ def cleanse_categorical_features(df, categorical_features):
         counts = df[feature].value_counts() # Get a Pandas series of all the values for
         single_values = counts[counts == 1] # Get values for this categorical feature unique to 1 row
         df[feature].loc[(df[feature].isin(list(single_values.index)))] = "Other" # Replace unique instances of categorical values with "Other"
-        #df[feature] = np.where(df[feature].isin(list(single_values.index)), "Other", df[feature])
     return df
 
-def vec_categorical_features(df, categorical_features):
+def classify_cat_features(df, cat_features):
     '''
-    Converts categorical features to one-hot encoded format (i.e. vectorization) and appends to the dataframe
-    :param df: A Pandas dataframe
-    :param categorical_features: The names of the categorical features to encode
-    :return: dataframe containing one-hot encoded features, list of one-hot encoded feature names
+    Classify categorical features as either single- or multi-valued.
+    :param df: Pandas dataframe
+    :param cat_features: List of categorical features
+    :return: list of single-valued categorical features, list of multi-valued categorical features
     '''
-    vec_categorical_features = []
-    for feature in categorical_features:
+    def classify_features(client_df):
+        '''
+        Helper function for categorical feature classification, distributed across clients.
+        :param client_df: Dataframe with 1 client's records
+        '''
+        for feature in cat_features:
+            # If this feature takes more than 1 value per client, move it to the list of multi-valued features
+            if client_df[feature].nunique() > 1:
+                sv_cat_features.remove(feature)
+                mv_cat_features.append(feature)
+                return
+
+    sv_cat_features = cat_features  # First, assume all categorical features are single-valued
+    mv_cat_features = []
+    df.groupby('ClientID').apply(classify_features)
+    return sv_cat_features, mv_cat_features
+
+def vec_multi_value_cat_features(df, mv_cat_features):
+    '''
+        Converts multi-valued categorical features to vectorized format and appends to the dataframe
+        :param df: A Pandas dataframe
+        :param mv_categorical_features: The names of the categorical features to vectorize
+        :return: dataframe containing vectorized features, list of vectorized feature names
+        '''
+    mv_vec_categorical_features = []
+    for feature in mv_cat_features:
         df_temp = pd.get_dummies(df[feature], prefix=feature)  # Create temporary dataframe of this feature vectorized
         df = pd.concat((df, df_temp), axis=1)  # Concatenate temp one hot dataframe with original dataframe
         df = df.drop(feature, axis=1)  # Drop the original feature
         vectorized_headers_list = list(df_temp)
         for i in range(len(vectorized_headers_list)):
-            vec_categorical_features.append(vectorized_headers_list[i]) # Keep track of vectorized features
-    return df, vec_categorical_features
+            mv_vec_categorical_features.append(vectorized_headers_list[i])  # Keep track of vectorized features
+    return df, mv_vec_categorical_features
 
+def vec_single_value_cat_features(df, sv_cat_features):
+    '''
+    Converts single-valued categorical features to one-hot encoded format (i.e. vectorization) and appends to the dataframe.
+    Keeps track of a mapping from feature indices to categorical values, for interpretability purposes.
+    :param df: A Pandas dataframe
+    :param sv_cat_features: The names of the categorical features to encode
+    :return: dataframe containing one-hot encoded features, list of one-hot encoded feature names
+    '''
+    vec_cat_features = []
+    cat_feature_idxs = [df.columns.get_loc(c) for c in sv_cat_features if c in df]   # List of categorical column indices
+    cat_value_names = {}    # Dictionary of categorical feature indices and corresponding names of feature values
+    for i in range(len(sv_cat_features)):
+        df_temp = pd.get_dummies(df[sv_cat_features[i]], prefix=sv_cat_features[i])  # Create temporary dataframe of this feature vectorized
+        value_names = [name[(name.index('_') + 1):] for name in df_temp.columns]    # Get list of values of this
+        cat_value_names[cat_feature_idxs[i]] = value_names
+        df = pd.concat((df, df_temp), axis=1)  # Concatenate temp one hot dataframe with original dataframe
+        df = df.drop(sv_cat_features[i], axis=1)  # Drop the original feature
+        vec_cat_features.extend(df_temp.columns)  # Keep track of vectorized feature columns
+    return df, vec_cat_features, cat_feature_idxs, cat_value_names
 
 def process_timestamps(df):
     '''
@@ -63,54 +105,16 @@ def process_timestamps(df):
 
 def remove_n_weeks(df, n_weeks, gt_end_date):
     '''
-    Remove records from the
-    :param df:
-    :param n_weeks:
-    :param gt_end_date:
-    :return:
+    Remove records from the dataframe that have timestamps in the n weeks leading up to the ground truth date
+    :param df: Pandas dataframe
+    :param n_weeks: number of recent weeks to remove records
+    :param gt_end_date: the date used for ground truth calculation
+    :return: updated dataframe with the relevant rows removed
     '''
     train_end_date = gt_end_date - timedelta(days=(n_weeks * 7))
     df = df[df['ServiceStartDate'] <= train_end_date]
     df.loc[df['ServiceEndDate'] > train_end_date, ['ServiceEndDate']] = train_end_date
-    #df['ServiceEndDate'] = np.where(df['ServiceEndDate'] > train_end_date, pd.to_datetime(train_end_date, infer_datetime_format=True, errors='coerce'), df['ServiceEndDate'])
     return df
-
-
-def make_start_end_features(df, noncategorical_features, timed_service_features, features_to_drop_last):
-    '''
-    Create and add features for start and end times of certain features
-    :param df: a Pandas dataframe
-    :param noncategorical_features: list of noncategorical features in the dataset
-    :param timed_service_features: list of service features that occur at events spaced in time
-    :return: the updated dataframe, the updated list of noncategorical features
-    '''
-    # Replace null ServiceEndDate entries with today's date. Assumes client is receiving ongoing services.
-    df['ServiceEndDate'] = np.where(df['ServiceEndDate'].isnull(), pd.to_datetime('today'), df['ServiceEndDate'])
-
-    # Create a new feature for the start and end date of each service feature
-    for feature in timed_service_features:
-        feature_start_name = feature + 'StartDate'
-        feature_end_name = feature + 'EndDate'
-        df[feature_start_name] = np.where(df['ServiceType'] == feature, df['ServiceStartDate'], 0)
-        df[feature_end_name] = np.where(df['ServiceType'] == feature, df['ServiceEndDate'], 0)
-        noncategorical_features.extend([feature_start_name, feature_end_name])
-        features_to_drop_last.extend([feature_start_name, feature_end_name])
-    return df, noncategorical_features, features_to_drop_last
-
-def calculate_length_features(df, timed_service_features):
-    '''
-    Create features for total length of time for features with start and end dates
-    :param df: a Pandas dataframe
-    :param timed_service_features: Service features whose start and end dates were calculated in this script
-    :return:
-    '''
-    seconds_per_day = 60 * 60 * 24 # 60sec/min * 60min/hr * 24hr/day
-    length_features = [] # Keep track of features identifying a time duration
-    for feature in timed_service_features:
-        length_feature_name = 'LengthOf' + feature + 'Days'
-        df[length_feature_name] = (df[feature + 'EndDate'] - df[feature + 'StartDate']).dt.total_seconds() / seconds_per_day
-        length_features.append(length_feature_name)
-    return df, length_features
 
 def create_service_num_features(df, counted_service_features):
     '''
@@ -215,7 +219,7 @@ def calculate_client_features(df, end_date):
         for row in client_df.itertuples():
             stay_start = getattr(row, 'ServiceStartDate')
             stay_end = min(getattr(row, 'ServiceEndDate'), end_date) # If stay is ongoing through end_date, set end of stay as end_date
-            if stay_start != last_start:
+            if (stay_start != last_start) and (getattr(row, 'ServiceType') == 'Stay'):
                 total_stays += (stay_end.date() - stay_start.date()).days + (stay_start.date() != last_end.date())
                 last_end = stay_end
                 last_start = stay_start
@@ -229,13 +233,13 @@ def calculate_client_features(df, end_date):
     df['TotalStays'] = 0    # Create columns for stays and income total
     df['IncomeTotal'] = 0
     df['MonthlyAmount'] = pd.to_numeric(df['MonthlyAmount'])
-    df_temp = df.loc[(df['ServiceType'] == 'Stay')]
+    df_temp = df.copy()
     df_temp = df_temp.groupby('ClientID').apply(client_features)
     df_temp = df_temp.droplevel('ClientID', axis='index')
     df.update(df_temp)  # Update all rows with corresponding stay length and total income
     return df
 
-def aggregate_df(df, noncategorical_features, vec_categorical_features, numerical_service_features):
+def aggregate_df(df, noncategorical_features, vec_mv_categorical_features, vec_sv_categorical_features, numerical_service_features):
     '''
     Build a dictionary of columns and arguments to feed into the aggregation function, and aggregate the dataframe
     :param df: a Pandas dataframe
@@ -252,8 +256,12 @@ def aggregate_df(df, noncategorical_features, vec_categorical_features, numerica
     # Create a dictionary of column names and function names to pass into the groupby function
     for i in range(len(non_categorical_features)):
         grouping_dictionary[non_categorical_features[i]] = 'first'  # Group noncategorical features by first occurrence
-    for i in range(len(vec_categorical_features)):
-        temp_dict[vec_categorical_features[i]] = 'max'  # Group one hot features by max value
+    for i in range(len(vec_sv_categorical_features)):
+        temp_dict[vec_sv_categorical_features[i]] = 'first'  # Group one hot features by max value
+    grouping_dictionary = {**grouping_dictionary, **temp_dict}
+    temp_dict = {}
+    for i in range(len(vec_mv_categorical_features)):
+        temp_dict[vec_mv_categorical_features[i]] = 'max'  # Group single categorical features by first value
     grouping_dictionary = {**grouping_dictionary, **temp_dict}
     temp_dict = {}
     for i in range(len(numerical_service_features)):
@@ -272,6 +280,7 @@ run_start = datetime.today()
 input_stream = open(os.getcwd() + "/config.yml", 'r')
 config = yaml.full_load(input_stream)
 categorical_features = config['DATA']['CATEGORICAL_FEATURES']
+sv_categorical_features = config['DATA']['SINGLE_VALUE_CATEGORICAL_FEATURES']
 noncategorical_features = config['DATA']['NONCATEGORICAL_FEATURES']
 features_to_drop_last = config['DATA']['FEATURES_TO_DROP_LAST']
 N_WEEKS = config['DATA']['N_WEEKS']     # Weeks in advance to predict label
@@ -294,9 +303,6 @@ noncategorical_features.append('HasFamily')
 print("Convert yes/no categorical features to boolean")
 df, categorical_features, noncategorical_features = convert_yn_to_boolean(df, categorical_features, noncategorical_features)
 
-# Create a new feature for start and end of each service type that is accessed over multiple timeframes
-#print("Adding features for service start/end dates.")
-#df, noncategorical_features, features_to_drop_last = make_start_end_features(df, noncategorical_features, config['DATA']['TIMED_SERVICE_FEATURES'], features_to_drop_last)
 # Replace null ServiceEndDate entries with today's date. Assumes client is receiving ongoing services.
 df['ServiceEndDate'] = np.where(df['ServiceEndDate'].isnull(), pd.to_datetime('today'), df['ServiceEndDate'])
 
@@ -328,25 +334,42 @@ df = df.set_index('ServiceStartDate')
 print("Cleansing categorical features of unique values.")
 df = cleanse_categorical_features(df, categorical_features)
 
-# Vectorize the categorical features
-print("Vectorizing categorical features.")
-df, vec_categorical_features = vec_categorical_features(df, categorical_features)
+print("Separating multi and single-valued categorical features.")
+sv_cat_features, mv_cat_features = classify_cat_features(df, categorical_features)
+
+# Vectorize the multi-valued categorical features
+print("Vectorizing multi-valued categorical features.")
+df, vec_mv_cat_features = vec_multi_value_cat_features(df, mv_cat_features)
 
 # Amalgamate rows to have one entry per client
 print("Grouping the dataframe.")
-df_unique_clients = aggregate_df(df, noncategorical_features, vec_categorical_features, numerical_service_features)
+df_clients = aggregate_df(df, noncategorical_features, vec_mv_cat_features, sv_cat_features, numerical_service_features)
 
 # Drop unnecessary features
 print("Dropping unnecessary features.")
 for column in features_to_drop_last:
-    df_unique_clients.drop(column, axis=1, inplace=True)
+    if column in df_clients.columns:
+        df_clients.drop(column, axis=1, inplace=True)
 
 # Replace all instances of NaN in the dataframe with 0
-df_unique_clients.fillna(0, inplace=True)
+df_clients.fillna("Unknown", inplace=True)
 
-# Save vectorized data
+# Vectorize single-valued categorical features. Keep track of feature names and values.
+print("Vectorizing single-valued categorical features.")
+df_ohe_clients, vec_sv_cat_features, sv_cat_feature_idxs, sv_cat_values = vec_single_value_cat_features(df_clients, sv_cat_features)
+
+# Save processed dataset
 print("Saving data.")
-df_unique_clients.to_csv(config['PATHS']['VECTORIZED_DATA'], sep=',', header=True)
+df_clients.to_csv(config['PATHS']['PROCESSED_DATA'], sep=',', header=True)
+df_ohe_clients.to_csv(config['PATHS']['PROCESSED_OHE_DATA'], sep=',', header=True)
+
+# For producing interpretable results with categorical data:
+interpretability_info = {}
+interpretability_info['VEC_SV_CAT_FEATURES'] = vec_sv_cat_features
+interpretability_info['SV_CAT_FEATURE_IDXS'] = sv_cat_feature_idxs
+interpretability_info['SV_CAT_VALUES'] = sv_cat_values
+with open(config['PATHS']['INTERPRETABILITY'], 'w') as file:
+    documents = yaml.dump(interpretability_info, file)
 
 # Log some useful stats
 print("# clients in last year meeting homelessness criteria = ", num_pos)
