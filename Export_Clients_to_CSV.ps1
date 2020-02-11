fa23d75b58db434527d370a144152e3882a624f7
bvanberl <bvanberl@uwo.ca>
2020-02-11 12:46:42 -0500

    Began composing readme. Included subfolders ignored in previous commits. Added scripts to pull raw data from HIFIS database.

---
+++ Export_Clients_to_CSV.ps1
@@ -0,0 +1,34 @@
+﻿##########################################################################################################
+##
+##   Script to export HIFIS Data for consumption by the preprocessing script.
+##
+##
+##   This script will run the SQLCMD utility to generate two CSV files
+##   Source SQL Script is found at src\data\ClientExport.sql
+##   csv file is saved to data\raw\HIFIS_Clients.csv
+##
+#########################################################################################################
+
+Import-Module SQLPS -DisableNameChecking
+
+$VerbosePreference = "SilentlyContinue"
+
+## Database Instance Name
+$instanceName =  [Instance Name goes here] 
+
+# SQL Scripts
+$clientScript = "src\data\ClientExport.sql" # Path to SQL Script
+
+
+# Output Files
+$clientOutputFile = "data\raw\HIFIS_Clients.csv"
+
+
+# export client data to csv file
+Invoke-Sqlcmd -InputFile $clientScript -ServerInstance $instanceName  | Export-Csv -Path $clientOutputFile -NoTypeInformation
+
+
+
+
+
+EXIT
