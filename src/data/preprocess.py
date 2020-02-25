203483dabbb9df259497cedbe974582104448b15
bvanberl <bvanberl@uwo.ca>
2020-02-25 16:31:24 -0500

    Preprocessing with just SPDAT questions

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -449,6 +449,8 @@ def preprocess(n_weeks=None, include_gt=True, calculate_gt=True, classify_cat_fe
             df_clients = df_clients.join(spdat_df, how='inner')      # Add SPDAT data, but only take clients with SPDATs
         else:
             df_clients = df_clients.join(spdat_df, how='left')      # Add SPDAT data for clients with SPDATs
+        if config['DATA']['SPDAT']['SPDAT_CLIENTS_ONLY']:
+            df_clients = df_clients[list(spdat_df.columns)]   # Only features will be SPDAT questions
         if classify_cat_feats:
             noncategorical_feats += noncat_spdat_feats
             sv_cat_feats += sv_cat_spdat_feats
