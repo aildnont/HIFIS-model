a63c434bc2a4526bafb564927314aa76f9ef0940
bvanberl <bvanberl@uwo.ca>
2020-03-12 13:36:17 -0400

    Prevent sorting of columns in concatenating new and old batch predictions

--- src/predict.py
+++ src/predict.py
@@ -126,7 +126,7 @@ def trending_prediction(data_path=None):
     # If previous prediction file exists, load it and append the predictions we just made.
     if os.path.exists(trending_pred_path):
         prev_results_df = pd.read_csv(trending_pred_path)
-        results_df = pd.concat([prev_results_df, results_df], axis=0)
+        results_df = pd.concat([prev_results_df, results_df], axis=0, sort=False)
 
     # Save the updated trend analysis prediction spreadsheet
     results_df.to_csv(trending_pred_path, columns=col_names, index_label=False, index=False)
